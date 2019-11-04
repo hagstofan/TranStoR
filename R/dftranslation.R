0e8a49fdec1c819ec5f78e0dab2d0a8416cb6e4f
Garðar Páll Gíslason <GardarG@hagstofa.local>
2019-11-04 14:15:29 +0000

    Requirements

--- R/dftranslation.R
+++ R/dftranslation.R
@@ -12,9 +12,7 @@
 #' list of dataframes, first is a dataframe with the rules and labels for each rule. The rest is data frames of each values or output from subquery and case statement.
 #' @export
 dfTranslation <- function(df,demo,dbengine,server,databasename){
-    
-  # býr til röð af id fyrir for loopuna
-  var <- seq(1, nrow(df))
+  
   
   # check if stringr is in use
   if("stringr" %in% (.packages())==FALSE)
@@ -27,16 +25,26 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
     stop("Check the columns name")
   }
   
-  df["new"] <- var
+  # Making a list which is the output
   dfList <- list()
+  
+  # Making listnumber begin second list
   listnr <-2
+  
+  # Make sequence for forloop
+  var <- seq(1, nrow(df))
+  
+  # Making new collumn for data frame
+  df["new"] <- var
+  
+  # Making data frame from every subquery
   for(val in var)
   {
     
     # Checking if each line has Select in the string
-    #if(grepl("SELECT",dflist[[1]][val,1], perl=TRUE)==TRUE)
     if(grepl("SELECT",df[val,1], perl=TRUE)==TRUE)
     {
+      # Because this is 
       # check the database engine
       if (dbengine ==2) {
         engine<- 'PostgreSQL'
@@ -51,21 +59,26 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
         stop("This package need RODBC package")
       }
       # Count SELECT. If there are two or more SELECT words it get special treatment
-      #if(length(grep("SELECT", dflist[[1]][val,1],perl=TRUE))>1)
-      #if(str_count(dflist[[1]][val,1],"SELECT")>1)
       if(str_count(df[val,1],"SELECT")>1)
       {
-        #mlines <-regmatches(dflist[[1]][val,1], gregexpr("\\(.*?\\)", dflist[[1]][val,1]))[[1]]
+        # Split sentence into miny sentences
         mlines <-regmatches(df[val,1], gregexpr("\\(.*?\\)", df[val,1]))[[1]]
+        # Make sequence of mlines
         nlines <- seq(1, length(mlines))
         
+        # Go through every line in the sequence
         for(n in nlines)
         {
+          # Check if this line has Select inside the line
           if(grepl("SELECT",mlines[n],perl = T)==T)
           {
+            # Make a line
             li <-mlines[n]
+            # Mae an database connection
             databases <- odbcDriverConnect(paste0("DRIVER={",engine,"}; SERVER=",server,"; trusted_connection=true;database=",databasename))
+            # Try to get a data frame
             d1 <- sqlQuery(databases,li, stringsAsFactors = FALSE)
+            # Close 
             odbcClose(databases)
             if(grepl("[Microsoft][ODBC SQL Server Driver][SQL Server]",d1[[1]][1]))
             {
@@ -76,9 +89,7 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
               li <- gsub("\\(","\\\\(",li)
               li <- gsub("\\)","\\\\)",li)
             }
-            # dfList[[listnr]] <-assign(gsub("(.*?SELECT )|(FROM .*)","",li,perl = TRUE),d1)
             dfList[[listnr]] <-d1
-            #dflist[[1]][val,1] <-gsub(li,paste(d1,sep = ","),dflist[[1]][val,1],perl = TRUE)
             df[val,1] <-gsub(li,paste('dfList[[',listnr,']]',sep = ''),df[val,1])
             d1
             rm(d1)
@@ -92,14 +103,12 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
       # If count is one
       else
       {
-        #li <-gsub(".*\\((?=SELECT)|\\)\\sAND+.*|\\)\\sOR+.*|\\)$", "",dflist[[1]][val,1], perl = TRUE)
         li <-gsub(".*\\((?=SELECT)|\\)\\sAND+.*|\\)\\sOR+.*|\\)$", "",df[val,1], perl = TRUE)
         databases <- odbcDriverConnect(paste0("DRIVER={",engine,"}; SERVER=",server,"; trusted_connection=true;database=",databasename))
         d1 <- sqlQuery(databases,li, stringsAsFactors = FALSE)
         odbcClose(databases)
         if(grepl("[Microsoft][ODBC SQL Server Driver][SQL Server]",d1[[1]][1]))
         {
-          #li <-gsub(".*\\((?=SELECT)|\\)$", "",dflist[[1]][val,1], perl = TRUE)
           li <-gsub(".*\\((?=SELECT)|\\)$", "",df[val,1], perl = TRUE)
           databases <- odbcDriverConnect(paste0("DRIVER={",engine,"}; SERVER=",server,"; trusted_connection=true;database=",databasename))
           d1 <- sqlQuery(databases,li, stringsAsFactors = FALSE)
@@ -110,9 +119,7 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
         li <- gsub("\\*","\\\\*",li)
         li <- gsub("\\+","\\\\+",li)
         
-        # dfList[[listnr]] <-assign(gsub("(.*?SELECT )|( FROM .*)","",li,perl = TRUE),d1)
         dfList[[listnr]] <-d1
-        #dflist[[1]][val,1] <-gsub(li,paste(d1,sep = ","),dflist[[1]][val,1],perl = TRUE)
         df[val,1] <-gsub(li,paste('dfList[[',listnr,']]',sep = ''),df[val,1],perl = TRUE)
         rm(d1)
         rm(li)
@@ -153,15 +160,27 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
       rm(li)
       rm(d1)
       
-      p1<-p1+1
+      
       listnr<-listnr+1
     }
   }
   rm(var)
   rm(val)
+  # remove from the data frame
   df <- subset(df, select = -c(new))
-  # Change RIGHT function 
-  df$rule <- gsub("RIGHT\\((.*?),(.*?)\\)" , "substr(\\1,nchar(\\1)-\\2+1,nchar(\\1))", df$rule)
+  # Change RIGHT function which is TRUE
+  df$rule <- gsub("RIGHT\\((.*?),(.*?)\\) = \\((.*?)\\)" , "\\(as.integer(substr(\\1,nchar(\\1)-\\2+1,nchar(\\1))) = as.vector\\(\\3\\)\\) = TRUE", df$rule)
+  df$rule <- gsub("RIGHT\\((.*?),(.*?)\\)=\\((.*?)\\)" , "\\(as.integer(substr(\\1,nchar(\\1)-\\2+1,nchar(\\1))) = as.vector\\(\\3\\)\\) = TRUE", df$rule)
+  # Change RIGHT function which is FALSE
+  df$rule <- gsub("RIGHT\\((.*?),(.*?)\\) != \\((.*?)\\)" , "\\(as.integer(substr(\\1,nchar(\\1)-\\2+1,nchar(\\1))) = as.vector\\(\\3\\)\\) = FALSE", df$rule)
+  df$rule <- gsub("RIGHT\\((.*?),(.*?)\\)!=\\((.*?)\\)" , "\\(as.integer(substr(\\1,nchar(\\1)-\\2+1,nchar(\\1))) = as.vector\\(\\3\\)\\) = FALSE", df$rule)
+  # Change " <> " for " != " for list
+  df$rule <- gsub("([a-zA-Z!@#$%^&*(),.?:{}|<>Þþ1-9_ÐðÁá+-]+) <> \\((.*?)\\)" , "(\\1 = (\\2)) = FALSE", df$rule)
+  # Change " <> " for " != " for list
+  df$rule <- gsub("([a-zA-Z!@#$%^&*(),.?:{}|<>Þþ1-9_ÐðÁá+-]+) <> \\'(.*?)\\'" , "(\\1 = '\\2') = FALSE", df$rule)
+  # Change " <> " for " != " for list
+  df$rule <- gsub("([a-zA-Z!@#$%^&*(),.?:{}|<>Þþ1-9_ÐðÁá+-]+) <> (.*?)" , "\\1 != \\2", df$rule)
+  
   # IS NULL
   df$rule[grep(" IS NULL", df$rule, perl = TRUE)] <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá+-]+)( ?) IS( ?) NULL','lapply(\\1,is.null) = TRUE',df$rule[grep(" IS NULL", df$rule, perl = TRUE)])
   df$rule[grep(" is null", df$rule, perl = TRUE)] <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá+-]+)( ?) is( ?) null','lapply(\\1,is.null) = TRUE',df$rule[grep(" is null", df$rule, perl = TRUE)])
@@ -170,8 +189,43 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
   df$rule[grep(" IS NOT NULL", df$rule, perl = TRUE)] <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá+-]+)( ?) IS( ?) NOT( ?) NULL','lapply(\\1,is.null) = FALSE',df$rule[grep(" IS NOT NULL", df$rule, perl = TRUE)])
   df$rule[grep(" is not null", df$rule, perl = TRUE)] <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá+-]+)( ?) is( ?) not( ?) null','lapply(\\1,is.null) = FALSE',df$rule[grep(" is not null", df$rule, perl = TRUE)])
   df$rule[grep(" is Not null", df$rule, perl = TRUE)] <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá+-]+)( ?) is( ?) Not( ?) null','lapply(\\1,is.null) = FALSE',df$rule[grep(" is Not null", df$rule, perl = TRUE)])
+  
   # set neitun fyrir framan orð
-  df$rule <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá]+) NOT IN ', '!\\1 %in% c', df$rule)
+  df$rule <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá]+) NOT IN \\((.*?)\\)', '(\\1 %in% c\\(\\2\\)\\)=FALSE', df$rule)
+  df$rule <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá]+) not in \\((.*?)\\)', '(\\1 %in% c\\(\\2\\)\\)=FALSE', df$rule)
+  # skipta út " IN " fyrir " %in% "
+  df$rule <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá]+) IN \\((.*?)\\)', '(\\1 %in% c\\(\\2\\)\\)=TRUE', df$rule)
+  df$rule <- gsub('([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-9_ÐðÁá]+) in \\((.*?)\\)', '(\\1 %in% c\\(\\2\\)\\)=TRUE', df$rule)
+  # skipta út "NOT LIKE " fyrir "grepl FALSE"
+  df$rule <- gsub('(\\w+) NOT LIKE ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)',"grepl(\\2,\\1)=FALSE", df$rule,perl = TRUE)
+  df$rule <- gsub('(\\w+) not like ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "grepl(\\2,\\1)=FALSE", df$rule,perl = TRUE)
+  # Changing " LIKE " for "grepl TRUE"
+  df$rule <- gsub('(\\w+) LIKE ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "grepl(\\2,\\1)=TRUE", df$rule)
+  df$rule <- gsub('(\\w+) like ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "grepl(\\2,\\1)=TRUE", df$rule)
+  
+  df$rule <- gsub("(\\w+)  ([0-9])" , "\\1 = \\2", df$rule)
+  # Changing SQL Length to R number of characters
+  df$rule <- gsub("LEN\\(" , "nchar\\(", df$rule)
+  df$rule <- gsub("len\\(" , "nchar\\(", df$rule)
+  # Changing NOT
+  df$rule <- gsub('(\\S+) NOT(.*?) &&','!(\\1\\2) &&', df$rule,perl = TRUE)
+  df$rule <- gsub('(\\S+) NOT(.*?$)','!(\\1\\2)', df$rule,perl = TRUE)
+  df$rule <- gsub('(\\S+) not(.*?$)','!(\\1\\2)', df$rule,perl = TRUE)
+  
+  # Changing MONTH to month
+  df$rule <- gsub('MONTH','month', df$rule,perl = TRUE)
+  # Changing substring
+  df$rule <- gsub('SUBSTRING','substr', df$rule,perl = TRUE)
+  
+  # Changing between
+  df$rule <- gsub('(\\w+) BETWEEN (\\w+) & (\\w+)','between(\\1,\\2,\\3)', df$rule,perl = TRUE)
+  df$rule <- gsub('(\\w+) between (\\w+) & (\\w+)','between(\\1,\\2,\\3)', df$rule,perl = TRUE)
+  df$rule <- gsub('(^.*)','!(\\1)', df$rule,perl = TRUE)
+  
+  # Changing < or > which has character on right side to number
+  df$rule <- gsub('< \'([0-9]+)\'','< \\1', df$rule,perl = TRUE)
+  df$rule <- gsub('> \'([0-9]+)\'','> \\1', df$rule,perl = TRUE)
+  
   # splitta "ANDis" fyrir "& is"
   df$rule <- gsub("ANDis", "& is", df$rule)
   df$rule <- gsub("ANDIS", "& is", df$rule)
@@ -192,35 +246,7 @@ dfTranslation <- function(df,demo,dbengine,server,databasename){
   # skipta úr ">==" fyrir ">="
   df$rule <- gsub(">==" , ">=", df$rule)
   
-  
-  
-  # skipta út " IN " fyrir " %in% "
-  df$rule <- gsub(" IN \\(" , " %in% c(", df$rule)
-  df$rule <- gsub(" in \\(" , " %in% c(", df$rule)
-  # skipta út " LIKE " fyrir " NOT LIKE "
-  df$rule <- gsub('(\\w+) NOT LIKE ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)',"!grepl(\\2,\\1)", df$rule,perl = TRUE)
-  df$rule <- gsub('(\\w+) not like ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "!grepl(\\2,\\1)", df$rule,perl = TRUE)
-  # skipta út " LIKE " fyrir " LIKE "
-  df$rule <- gsub('(\\w+) LIKE ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "grepl(\\2,\\1)", df$rule)
-  df$rule <- gsub('(\\w+) like ([a-zA-Z!@#$%^&*(),.?":{}|<>Þþ1-90_ÐðÁá+\'-]+)' , "grepl(\\2,\\1)", df$rule)
-  # skipta út " <> " fyrir " != "
-  df$rule <- gsub(" <> " , " != ", df$rule)
-  df$rule <- gsub("(\\w+)  ([0-9])" , "\\1 = \\2", df$rule)
-  df$rule <- gsub("LEN\\(" , "nchar\\(", df$rule)
-  df$rule <- gsub("len\\(" , "nchar\\(", df$rule)
-  # Skipa út NOT
-  df$rule <- gsub('(\\S+) NOT(.*?) &&','!(\\1\\2) &&', df$rule,perl = TRUE)
-  df$rule <- gsub('(\\S+) NOT(.*?$)','!(\\1\\2)', df$rule,perl = TRUE)
-  df$rule <- gsub('(\\S+) not(.*?$)','!(\\1\\2)', df$rule,perl = TRUE)
-  
-  # Skipta út MONTH
-  df$rule <- gsub('MONTH','month', df$rule,perl = TRUE)
-  # Skipta út substring
-  df$rule <- gsub('SUBSTRING','substr', df$rule,perl = TRUE)
-  
-  # Skipta út between
-  df$rule <- gsub('(\\w+) BETWEEN (\\w+) & (\\w+)','between(\\1,\\2,\\3)', df$rule,perl = TRUE)
-  df$rule <- gsub('(\\w+) between (\\w+) & (\\w+)','between(\\1,\\2,\\3)', df$rule,perl = TRUE)
   dfList[[1]] <- df
+  
   return(dfList)
   }
